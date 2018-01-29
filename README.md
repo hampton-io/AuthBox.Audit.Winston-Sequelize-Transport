cc97d7e279019e091e52661760438d3c8eef6eee
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-01-29 21:52:01 +0000

    Initial commit for creating winston sequelize transport

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # login.dfe.audit.winston-sequelize-transport
+
+Creates a winston sequelize transport. The following config options are required:
+
+```
+    "hostingEnvironment": {
+    "env": "dev"
+    },
+    "loggerSettings": {
+        "logLevel": "debug",
+        "applicationName": "MyApi",
+        "auditDb": {
+          "host" :"DBHostName",
+          "username":"db-login-name",
+          "password":"db-login-pwd",
+          "dialect":"mssql",
+          "name":"database-name",
+          "encrypt": true,
+          "schema": "dbo"
+        }
+  },
+```
+
+usage is as follows:
+
+```
+
+const winstonSequelizeTransport = require('login.dfe.audit.winston-sequelize-transport');
+
+
+const transport = new winstonSequelizeTransport(config);
+const loggerConfig = {
+  transports: [],
+};
+loggerConfig.transports.push(new WinstonTransportSequelize(options));
+const logger = new (winston.Logger)(loggerConfig);
+
+```
---
+++ test/index.test.js
@@ -0,0 +1,50 @@
+jest.mock('sequelize');
+jest.mock('winston-transport-sequelize');
+
+const index = require('./../src/index');
+
+describe('When getting the winston sequelize transport ', () => {
+
+  it('then null is returned if the config is not set', () => {
+    const configObject = {
+      loggerSettings:{
+        auditDb:{
+        }
+      }
+    };
+
+    const actual = index(configObject);
+
+    expect(actual).toBeNull();
+  });
+  it('then the transport object is returned if the config is correctly set', () => {
+    const sequelizeMock = require('sequelize');
+    const winstonMock = require('winston-transport-sequelize');
+
+    const configObject = {
+      hostingEnvironment: {
+        env: 'test'
+      },
+      loggerSettings:{
+        auditDb:{
+          name: 'test-db',
+          username: 'testPerson',
+          password: 'myPassword',
+          host: 'auditHost',
+          dialect: 'mssql',
+        },
+        applicationName: 'test application',
+      }
+    };
+
+    const actual = index(configObject);
+
+    expect(actual).not.toBeNull();
+    expect(winstonMock.mock.calls.length).toBe(1);
+    expect(winstonMock.mock.calls[0][0].application).toBe('test application');
+    expect(sequelizeMock.mock.calls.length).toBe(1);
+    expect(sequelizeMock.mock.calls[0][0]).toBe('test-db');
+    expect(sequelizeMock.mock.calls[0][1]).toBe('testPerson');
+    expect(sequelizeMock.mock.calls[0][2]).toBe('myPassword');
+  });
+});
